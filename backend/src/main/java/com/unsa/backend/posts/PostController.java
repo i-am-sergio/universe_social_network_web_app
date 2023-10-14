1c78072552b8235f86612dad683636eab093b7c4
sergio15dev <sergio15.dev@gmail.com>
2023-10-14 18:45:20 -0500

    add getPost and createPost endpoints

--- backend/src/main/java/com/unsa/backend/posts/PostController.java
+++ backend/src/main/java/com/unsa/backend/posts/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,14 +19,33 @@ public class PostController {
     @Autowired
     PostService postService;
 
-    // @GetMapping("/{id}")
-    // public ResponseEntity<PostModel> getPost(@PathVariable Long id) {
-    //     try {
-    //         return postService.getPostById(id)
-    //                 .map(post -> new ResponseEntity<>(post, HttpStatus.OK))
-    //                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    //     } catch (Exception e) {
-    //         return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-    //     }
-    // }
+    @GetMapping
+    public List<PostModel> getPosts(){
+        return postService.getPosts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostModel> getPost(@PathVariable Long id) {
+        try {
+            PostModel post = postService.getPostById(id);
+
+            if (post != null) {
+                return new ResponseEntity<>(post, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<PostModel> createPost(@RequestBody PostModel newPost) {
+        try {
+            PostModel createdPost = postService.createPost(newPost);
+            return new ResponseEntity<>(createdPost, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- backend/src/main/java/com/unsa/backend/posts/PostService.java
+++ backend/src/main/java/com/unsa/backend/posts/PostService.java
@@ -2,6 +2,7 @@ package com.unsa.backend.posts;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,17 @@ public class PostService {
     @Autowired
     PostRepository postRepository;
 
-    public List<PostModel> obtenerPublicaciones(){
+    public List<PostModel> getPosts(){
         return (ArrayList<PostModel>) postRepository.findAll();
     }
+
+    public PostModel getPostById(Long id) {
+        Optional<PostModel> optionalPost = postRepository.findById(id);
+        return optionalPost.orElse(null);
+    }
+
+    public PostModel createPost(PostModel newPost) {
+        return postRepository.save(newPost);
+    }
+
 }
