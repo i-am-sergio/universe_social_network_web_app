5b9de37d05ca0696bd73dc3219f3daaeacb0afa4
sergio15dev <sergio15.dev@gmail.com>
2023-10-15 10:18:22 -0500

    add updatePost and deletePost

--- backend/src/main/java/com/unsa/backend/posts/PostController.java
+++ backend/src/main/java/com/unsa/backend/posts/PostController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +22,18 @@ public class PostController {
     PostService postService;
 
     @GetMapping
-    public List<PostModel> getPosts(){
-        return postService.getPosts();
+    public ResponseEntity<List<PostModel>> getPosts() {
+        try {
+            List<PostModel> posts = postService.getPosts();
+
+            if (!posts.isEmpty()) {
+                return new ResponseEntity<>(posts, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping("/{id}")
@@ -35,7 +47,7 @@ public class PostController {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
 
@@ -45,7 +57,66 @@ public class PostController {
             PostModel createdPost = postService.createPost(newPost);
             return new ResponseEntity<>(createdPost, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updatePost(@PathVariable Long id, @RequestBody PostModel updatedPost) {
+        try {
+            PostModel existingPost = postService.getPostById(id);
+
+            if (existingPost != null) {
+                if (existingPost.getUserId().equals(updatedPost.getUserId())) {
+                    postService.updatePost(existingPost, updatedPost);
+                    return new ResponseEntity<>("Post updated!", HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>("Authentication failed", HttpStatus.FORBIDDEN);
+                }
+            } else {
+                return new ResponseEntity<>("Post not found", HttpStatus.NOT_FOUND);
+            }
+            
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<String> deletePost(@PathVariable Long postId) {
+        try{
+            if (postService.deletePost(postId)) {
+                return new ResponseEntity<>("Post eliminado correctamente", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("No se pudo encontrar el post con ID: " + postId, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @PutMapping("/{id}/like")
+    public ResponseEntity<String> likePost(@PathVariable Long id, @RequestBody LikeRequest likeRequest) {
+        try {
+            PostModel post = postService.getPostById(id);
+            if(post == null){
+                return new ResponseEntity<>("Post not found", HttpStatus.NOT_FOUND);
+            }
+
+            if (post.getLikes().contains(likeRequest.getUserId())) {
+                postService.removeLike(post, likeRequest.getUserId());
+                return new ResponseEntity<>("Post disliked", HttpStatus.OK);
+            } else {
+                postService.addLike(post, likeRequest.getUserId());
+                return new ResponseEntity<>("Post liked", HttpStatus.OK);
+            }
+        } catch (Exception e) {
+           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    /*
+     *  Get timeline posts.
+        Falta implementar el metodo getTimelinePosts()
+     */
 }
--- backend/src/main/java/com/unsa/backend/posts/PostService.java
+++ backend/src/main/java/com/unsa/backend/posts/PostService.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class PostService {
     @Autowired
@@ -23,6 +25,43 @@ public class PostService {
 
     public PostModel createPost(PostModel newPost) {
         return postRepository.save(newPost);
+    }
+
+    public void updatePost(PostModel existingPost, PostModel updatedPost) {
+        // Actualizar el post existente con los datos del post actualizado
+        existingPost.setDesc(updatedPost.getDesc());
+        existingPost.setLikes(updatedPost.getLikes());
+        existingPost.setImage(updatedPost.getImage());
+        // Guardar el post actualizado en la base de datos
+        postRepository.save(existingPost);
+    }
+
+    public boolean deletePost(Long postId) {
+        Optional<PostModel> postOptional = postRepository.findById(postId);
+        if (postOptional.isPresent()) {
+            postRepository.delete(postOptional.get());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Transactional
+    public void addLike(PostModel post, String userId) {
+        List<String> likes = post.getLikes();
+        if (!likes.contains(userId)) {
+            likes.add(userId);
+            postRepository.save(post);
+        }
+    }
+
+    @Transactional
+    public void removeLike(PostModel post, String userId) {
+        List<String> likes = post.getLikes();
+        if (likes.contains(userId)) {
+            likes.remove(userId);
+            postRepository.save(post);
+        }
     }
 
 }
