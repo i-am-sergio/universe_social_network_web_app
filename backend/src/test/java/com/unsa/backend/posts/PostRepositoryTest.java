260bc0fdd7b1de27481004201390d36501a94f48
ShinjiMC <nayapbrau@gmail.com>
2023-12-11 21:30:34 -0500

    First Jtest

--- backend/src/test/java/com/unsa/backend/posts/PostRepositoryTest.java
+++ backend/src/test/java/com/unsa/backend/posts/PostRepositoryTest.java
@@ -1,49 +1,26 @@
 package com.unsa.backend.posts;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import lombok.AllArgsConstructor;
-
-/**
- * This class contains unit tests for the PostRepository class.
- */
+@SpringBootTest
 @DisplayName("Test Repository")
-@DataJpaTest
-@AllArgsConstructor
-public class PostRepositoryTest {
-    
-    private final PostRepository postRepository;
+class PostRepositoryTest {
 
-    // private PostModel post;
+    @Autowired
+    private PostRepository postRepository;
 
     @BeforeEach
     void setup() {
         System.out.println("Setup...................");
-        // post = PostModel.builder()
-        //     .userId(1L)
-        //     .desc("Test description 1")
-        //     .build();
-    }
-
-    /**
-     * Test case for getting all posts from the repository.
-     */
-    @DisplayName("Test get all posts")
-    @Test
-    public void testGetPosts() {
-        // given
-        List<PostModel> posts = null; 
-        // when
-        posts = postRepository.findAll();
-        // then
-        assertNotNull(posts);
     }
 
     /**
@@ -51,13 +28,26 @@ public class PostRepositoryTest {
      */
     @DisplayName("Test get post by id")
     @Test
-    public void testGetPostById() {
+    void testGetPostById() {
         // given
         PostModel post = null;
         // when
         post = postRepository.findById(1L).orElse(null);
         // then
-        assertNotNull(post);
+        assertNull(post);
     }
 
+    /**
+     * Test case for getting all posts from the repository.
+     */
+    @DisplayName("Test get all posts")
+    @Test
+    void testGetPosts() {
+        // given
+        List<PostModel> posts = null;
+        // when
+        posts = postRepository.findAll();
+        // then
+        assertNotNull(posts);
+    }
 }
