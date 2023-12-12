20a73481ce8f906652ab440ce8d144b423c4cf14
ShinjiMC <nayapbrau@gmail.com>
2023-12-11 21:30:34 -0500

    First Jtest

--- backend/src/test/java/com/unsa/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/unsa/backend/BackendApplicationTests.java
@@ -2,12 +2,14 @@ package com.unsa.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
