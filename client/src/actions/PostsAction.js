e16f2936f0263904db241c1ca8f2ddcc10de0dee
ShinjiMC <nayapbrau@gmail.com>
2023-12-13 23:54:25 -0500

    Refactor PostRepositoryTest.java, ChatBox.jsx and PostAction.js

--- client/src/actions/PostsAction.js
+++ client/src/actions/PostsAction.js
@@ -4,11 +4,8 @@ export const getTimelinePosts = (id) => async (dispatch) => {
   dispatch({ type: "RETREIVING_START" });
   try {
     const data = await PostsApi.getTimelinePosts(id);
-    // console.log("Status Code:", data.status);
-    // console.log("data ---> ",data);
     dispatch({ type: "RETREIVING_SUCCESS", data: data });
   } catch (error) {
-    console.log("ERRRRRORRR => ", error);
     dispatch({ type: "RETREIVING_FAIL" });
   }
 };
