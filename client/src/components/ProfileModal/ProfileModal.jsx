de1adea28f51cb680e6e8a800a9957e391abc7e4
Nelzon Apaza <84747728+nelzonapa@users.noreply.github.com>
2023-12-15 12:51:59 -0500

    Aplying Simplify Conditional Expressions | Refactoring Techniques

--- client/src/components/ProfileModal/ProfileModal.jsx
+++ client/src/components/ProfileModal/ProfileModal.jsx
@@ -21,11 +21,10 @@ const ProfileModal = ({ modalOpened, setModalOpened, data }) => {
   };
 
   const onImageChange = (event) => {
-    if (event.target.files?.event.target.files[0]) {
+    if (event.target.files && event.target.files[0]) {
       let img = event.target.files[0];
-      event.target.name === "profileImage"
-        ? setProfileImage(img)
-        : setCoverImage(img);
+      const isProfileImage = event.target.name === "profileImage";
+      isProfileImage ? setProfileImage(img) : setCoverImage(img);
     }
   };
 
