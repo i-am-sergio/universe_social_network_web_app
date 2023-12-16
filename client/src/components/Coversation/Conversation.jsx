f2db5ad53ebbc31c44b7e8b66dd1cd6d770d3cd6
Lu0811 <lhuamanco@unsa.edu.pe>
2023-12-16 14:37:14 -0500

    Copia de proyecto

--- client/src/components/Coversation/Conversation.jsx
+++ client/src/components/Coversation/Conversation.jsx
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getUser } from "../../api/UserRequests";
+
 const Conversation = ({ data, currentUser, online }) => {
 
   const [userData, setUserData] = useState(null)
   const dispatch = useDispatch()
 
-  useEffect(()=> {
+  useEffect(() => {
 
-    const userId = data.members.find((id)=>id!==currentUser)
-    const getUserData = async ()=> {
-      try
-      {
-          const {data} =await getUser(userId)
-         setUserData(data)
-         dispatch({type:"SAVE_USER", data:data})
+    const userId = data.members.find((id) => id !== currentUser)
+    const getUserData = async () => {
+      try {
+        const { data } = await getUser(userId)
+        setUserData(data)
+        dispatch({ type: "SAVE_USER", data: data })
       }
-      catch(error)
-      {
+      catch (error) {
         console.log(error)
       }
     }
@@ -30,14 +29,14 @@ const Conversation = ({ data, currentUser, online }) => {
         <div>
           {online && <div className="online-dot"></div>}
           <img
-            src={userData?.profilePicture? process.env.REACT_APP_PUBLIC_FOLDER + userData.profilePicture : process.env.REACT_APP_PUBLIC_FOLDER + "defaultProfile.png"}
+            src={userData?.profilePicture ? process.env.REACT_APP_PUBLIC_FOLDER + userData.profilePicture : process.env.REACT_APP_PUBLIC_FOLDER + "defaultProfile.png"}
             alt="Profile"
             className="followerImage"
             style={{ width: "50px", height: "50px" }}
           />
-          <div className="name" style={{fontSize: '0.8rem'}}>
+          <div className="name" style={{ fontSize: '0.8rem' }}>
             <span>{userData?.firstname} {userData?.lastname}</span>
-            <span style={{color: online?"#51e200":""}}>{online? "Online" : "Offline"}</span>
+            <span style={{ color: online ? "#51e200" : "" }}>{online ? "Online" : "Offline"}</span>
           </div>
         </div>
       </div>
