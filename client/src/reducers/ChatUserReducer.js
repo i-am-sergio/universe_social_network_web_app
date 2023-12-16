0a095d03a7e731bf098ee8f5c87512f02c7e87d1
PaulParizacaMozo <pparizaca@unsa.edu.pe>
2023-12-16 17:57:35 -0500

    Refactor ChatUserReducer

--- client/src/reducers/ChatUserReducer.js
+++ client/src/reducers/ChatUserReducer.js
@@ -1,10 +1,12 @@
 const chatReducer = (state = { chatUsers: [], loading: false, error: false }, action) => {
     switch (action.type) {
-            case "SAVE_USER":
-                return ({...state, chatUsers: [...state.chatUsers, action.data]});
-            case "___":
-                return 0;
-            default:
-                return state
-    }} 
-export default chatReducer
+        case "SAVE_USER":
+            return { ...state, chatUsers: [...state.chatUsers, action.data] };
+        case "___":
+            return { chatUsers: [], loading: false, error: false };
+        default:
+            return state;
+    }
+};
+
+export default chatReducer;
